c21d939d35b4a97de98bc8083c7735832c4c9e2b
Petr Davydov <Davydov.atc@yandex.ru>
2023-04-28 17:22:36 +0300

    add unitest to reach 100% cover

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,23 @@
 import unittest
-from utils import arrs
+import utils.arrs as arrs
 
 
 class TestArrs(unittest.TestCase):
-
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
+        arr = [1, 2, 3, 4]
+        self.assertEqual(arrs.get(arr, 0), 1)
+        self.assertEqual(arrs.get(arr, -1), 4)
+        self.assertEqual(arrs.get(arr, 4), None)
+        self.assertEqual(arrs.get(arr, 4, 'default'), 'default')
+        self.assertEqual(arrs.get(arr, -5, 'default'), 'default')
 
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+    def test_my_slice(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(arrs.my_slice(arr), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.assertEqual(arrs.my_slice(arr, 0, 5), [1, 2, 3, 4, 5])
+        self.assertEqual(arrs.my_slice(arr, -5), [6, 7, 8, 9, 10])
+        self.assertEqual(arrs.my_slice(arr, 0, -5), [1, 2, 3, 4, 5])
+        self.assertEqual(arrs.my_slice(arr, 3, 7), [4, 5, 6, 7])
+        self.assertEqual(arrs.my_slice(arr, 3, -3), [4, 5, 6, 7])
+        self.assertEqual(arrs.my_slice([], 0, 1), [])
+        self.assertEqual(arrs.my_slice([], -1, 0), [])
